2df557c68643f815021bc97b4c83779392dac105
motilyfor <251684946@qq.com>
2022-10-19 14:27:27 +0800

    <feat>:hotspot and static ip
    hotspot and static ip test

--- entry/src/main/ets/ServiceAbility/service.ts
+++ entry/src/main/ets/ServiceAbility/service.ts
@@ -3,6 +3,7 @@ import wifi from '@ohos.wifi';
 import parameter from '@ohos.systemparameter'
 import LogUtil from '../common/utils/LogUtils'
 import bluetooth from '@ohos.bluetooth';
+//import geolocation from '@ohos.geolocation';
 
 const MODULE_TAG = "DS_TEST"
 
@@ -45,22 +46,29 @@ function scanWiFi(): boolean {
     return ret;
 }
 
-function connectWiFi(ssid: string, password: string) {
-    let apInfo = this.userSelectedAp.getApInfo();
+function connectWiFi(ssid: string, password: string, bssid: string) {
+    LogUtil.info(MODULE_TAG, "ssid:" + ssid + " bssid:" + bssid + " password:" + password)
     let connectParam: any = {
-        "ssid": apInfo.ssid,
-        "bssid": apInfo.bssid,
+        "ssid": ssid,
+        "bssid": bssid,
         "preSharedKey": password,
         "isHiddenSsid": false, // we don't support connect to hidden ap yet
-        "securityType": apInfo.securityType
+        "securityType": 3
     };
-
+    let ret = false;
     if (wifi.isConnected() === true) {
         wifi.disconnect();
+        LogUtil.info(MODULE_TAG + 'disconnect WiFi ret is ' + ret);
+        registerWiFiConnectionObserver((code: Number) => {
+            if (code === 0) {
+                ret = wifi.connectToDevice(connectParam);
+                unregisterWiFiConnectionObserver();
+            }
+        })
+    } else {
+        ret = wifi.connectToDevice(connectParam);
+        LogUtil.info(MODULE_TAG , 'connect WiFi ret is ' + ret);
     }
-
-    let ret = wifi.connectToDevice(connectParam);
-    LogUtil.info(MODULE_TAG , 'connect WiFi ret is ' + ret);
     return ret;
 }
 
@@ -68,7 +76,9 @@ function connectWiFi(ssid: string, password: string) {
  * Disconnect wifi
  */
 function disconnectWiFi() {
-    this.setUserSelectedAp(null);
+    if (!isWiFiConnected()) {
+        return true;
+    }
     let ret = wifi.disconnect();
     LogUtil.info(MODULE_TAG , 'disconnect WiFi result is : ' + ret);
     return ret;
@@ -80,6 +90,14 @@ function enableBluetooth(): boolean {
     return ret;
 }
 
+function registerWiFiConnectionObserver(callback) {
+  LogUtil.info(MODULE_TAG + 'start register wifi connection observer');
+  wifi.on('wifiConnectionChange', callback);
+}
+function unregisterWiFiConnectionObserver() {
+    LogUtil.info(MODULE_TAG + 'start unregister wifi connection observer');
+    wifi.off('wifiConnectionChange');
+}
 export default {
     onStart() {
         LogUtil.info(MODULE_TAG , 'ServiceAbility onStart');
@@ -92,14 +110,41 @@ export default {
     }
 };
 
-for (;; ) {
 
+//// TODO gps test
+//var requestInfo = {'scenario': 0x301, 'timeInterval': 0, 'distanceInterval': 0, 'maxAccuracy': 0};
+//
+//var locationChange = (location) => {
+//    LogUtil.info(MODULE_TAG ,'locationChanger: data: ' + JSON.stringify(location));
+//};
+//
+//try {
+//    LogUtil.info(MODULE_TAG, 'enable location');
+//    geolocation.enableLocation();
+//} catch (e) {
+//    LogUtil.error(MODULE_TAG , "enableLocation error: " + e);
+//}
+//
+//geolocation.on('locationChange', requestInfo, locationChange);
+
+//for(;;) {
+//    LogUtil.info(MODULE_TAG ,'getCurrentLocation: data');
+//    var loca = geolocation.getCurrentLocation();
+//    LogUtil.info(MODULE_TAG ,'getCurrentLocation: data: ' + JSON.stringify(loca));
+//    sleep(1000)
+//}
+
+
+
+for (;; ) {
     LogUtil.info(MODULE_TAG , 'get_parameter');
     let op = "default"
     try {
         op = parameter.getSync("wifi.op", "default");
-        parameter.setSync("wifi.op", "default");
-        LogUtil.info(MODULE_TAG, ":op:" + op);
+        if(op != "default") {
+            parameter.setSync("wifi.op", "default");
+            LogUtil.info(MODULE_TAG, ":op:" + op);
+        }
     } catch (e) {
         LogUtil.error(MODULE_TAG , "set unexpected error: " + e);
     }
@@ -116,10 +161,12 @@ for (;; ) {
             break;
         case 'connect':
             let ssid = parameter.getSync("wifi.ssid", "null")
-            let pws = parameter.getSync("wifi.psw", "null")
+            let pw = parameter.getSync("wifi.pw", "null")
+            let bssid = parameter.getSync("wifi.bssid", "null")
             if (ssid != "null") {
-                connectWiFi(ssid, pws);
+                connectWiFi(ssid, pw, bssid);
             }
+            break;
         case 'disconnect':
             disconnectWiFi();
             break
@@ -128,6 +175,60 @@ for (;; ) {
             break;
         case 'isconnect':
             isWiFiConnected();
+            break;
+        case 'getipinfo':
+            var info = wifi.getDeviceConfigs();
+            LogUtil.info(MODULE_TAG, 'ipinfo:', JSON.stringify(info));
+            break;
+        case 'staticip':
+            let ipconfig : wifi.IpInfo = {
+                "ipAddress": 3232261478, //192.168.3.9
+                "gateway": 3232261568, //192.168.101.192
+                "dnsServers": [3232261568,134743044]
+            }
+            let connectParam: wifi.WifiDeviceConfig = {
+                "ssid": "Redmi",
+                "bssid": "86:be:18:85:29:59",
+                "preSharedKey": "hycasdfgh",
+                "isHiddenSsid": false, // we don't support connect to hidden ap yet
+                "securityType": 3,
+                "ipType": 1,
+                "staticIp": ipconfig
+            };
+            LogUtil.info(MODULE_TAG, 'addDeviceConfig:');
+            wifi.addDeviceConfig(connectParam)
+            break;
+        case 'updatenetwork':
+            let config : wifi.IpInfo = {
+                "ipAddress": 3232236297, //192.168.3.9
+                "gateway": 3232236289, //192.168.3.1
+                "dnsServers": [3232236289,134743044] //192.168.3.1 ,8,8,4,4
+            };
+            let param: wifi.WifiDeviceConfig = {
+                "ssid": "yqstest",
+                "bssid": "f4:8e:92:02:8f:c0",
+                "preSharedKey": "qweqweqwe",
+                "isHiddenSsid": false, // we don't support connect to hidden ap yet
+                "securityType": 3,
+                "ipType": 1,
+                "staticIp": config
+            };
+            wifi.updateNetwork(param)
+            break;
+        case 'sethostconfig':
+            let hostconfig : wifi.HotspotConfig = {
+                "ssid" : "hottest",
+                "securityType": 3,
+                "band" : 1,
+                "preSharedKey" : "qweqweqwe"
+            }
+            let ret = wifi.setHotspotConfig(hostconfig);
+            LogUtil.error(MODULE_TAG, "set hotspot:" + ret);
+            break;
+        case 'enablehot':
+            let res = wifi.enableHotspot();
+            LogUtil.error(MODULE_TAG, "enable hotspot failed" + res);
+            break;
         default:
             break;
     }
